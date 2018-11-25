911ccfa872f22478c7bb8012c60ad9058b0d2c44
radekbor <radoslaw.borowiecki@gmail.com>
2018-11-25 12:51:49 +0100

    Add unit test for reducer

---
+++ src/test/scala/org/radekbor/IntSumReaderTest.scala
@@ -0,0 +1,25 @@
+package org.radekbor
+
+import org.apache.hadoop.io.Text
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.FunSuite
+import scala.collection.JavaConverters._
+
+class IntSumReaderTest extends FunSuite with MockFactory {
+
+  val reducer = new IntSumReader()
+
+  test("should extract marks from line") {
+    val key = new Text("2")
+    val value1 = new MyPair(3, 1)
+    val value2 = new MyPair(2, 1)
+    val values = Iterable.newBuilder.+=(value1).+=(value2).result()
+
+    val context = mock[reducer.Context]
+    (context.write _).expects(new Text("2"), new MyPair(5, 2)).once()
+
+    reducer.reduce(key, values.asJava, context)
+
+  }
+
+}
